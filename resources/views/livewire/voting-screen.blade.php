35f7c3c65f4b7d67ca1def8b34c19d8845f81275
Kzrtt <99203878+Kzrtt@users.noreply.github.com>
2025-05-07 17:56:20 -0300

    [hotfix]: resolvendo problema na interface

--- resources/views/livewire/voting-screen.blade.php
+++ resources/views/livewire/voting-screen.blade.php
@@ -90,11 +90,11 @@
             selected: @entangle('selectedProject')
         }"
     >
-        <div class="mt-8 w-full grid grid-cols-4 gap-x-10">
+        <div class="flex flex-row mt-8 w-full grid grid-cols-4 gap-x-10">
             @foreach($projects as $project)
                 <div 
                     @click="selected = {{ $project['prj_id'] }}; $wire.selectProject({{ $project['prj_id'] }})"
-                    class="bg-gray-100 relative rounded-lg shadow hover:shadow-lg hover:scale-[1.01] hover:cursor-pointer transition-all duration-200"
+                    class="bg-gray-100 w-full relative rounded-lg shadow hover:shadow-lg hover:scale-[1.01] hover:cursor-pointer transition-all duration-200"
                 >
                     <div class="flex items-center justify-center w-20 h-20 bg-white rounded-full absolute top-6 right-3">
                         <div class="flex items-center justify-center w-17 h-17 bg-tertiary-200/80 rounded-full">
